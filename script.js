fb13301f53451b39491f35e773a79b6ab1cd3525
XanderOxeda <xanderoxeda@gmail.com>
2023-01-18 20:22:29 -0500

    Cambio en el orden de ejecución

--- script.js
+++ script.js
@@ -4,19 +4,17 @@ let gameTurn = 1;
 let symbolPlayerOne = 0;
 let symbolPlayerTwo = 0;
 
-tiles.forEach((tile,i)=>{
+tiles.forEach((tile,i)=>{ 
     tile.addEventListener("click", ()=>{
-        
-        const playerOne = [];
-        const playerTwo = [];
-        console.log("XO",i)
-
-        tile.innerText = symbols[gameTurn];
         if (gameTurn == 1) {
             gameTurn = 0;
         }else{
             gameTurn=1
         }
+        console.log("XO",i)
+
+        tile.innerText = symbols[gameTurn];
+        
 
     })
 });
